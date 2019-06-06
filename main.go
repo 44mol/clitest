a6aa4d18d74bcb4dfe553ff7eead3af7bd151230
44mol <moriken3.14@gmail.com>
2019-06-07 03:12:17 +0900

    add flags

--- main.go
+++ main.go
@@ -13,10 +13,38 @@ func main() {
 	app.Usage = "make an explosive entrance"
 	app.Version = "0.0.1"
 
+	os.Setenv("SAMPLE_ENV", "sample env dayo")
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "lang, l",
+			Value: "english",
+			Usage: "language for the greeting",
+		},
+		cli.StringFlag{
+			Name:  "meridian, m",
+			Value: "AM",
+			Usage: "meridian ofr the greeting",
+		},
+		cli.StringFlag{
+			Name:  "time, t",
+			Value: "07:00",
+			Usage: "`your time` for the greeting",
+		},
+		cli.StringFlag{
+			Name:   "aaa, a",
+			Value:  "sample",
+			EnvVar: "SAMPLE_ENV",
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
 		fmt.Println("--Action--")
 
-		fmt.Print("c.NArg()		: %+v\n", c.NArg())
+		fmt.Printf("c.NArg()		: %+v\n", c.NArg())
+		fmt.Printf("c.Args()		: %+v\n", c.Args())
+		fmt.Printf("c.Args().get(0)	: %+v\n", c.Args().Get(0))
+		fmt.Printf("c.FlagNames		: %+v\n", c.FlagNames)
 		return nil
 	}
 
